6cb6fe77deba33f61d08de56ce820adb917a7f0b
Clyde Cox <cjcox17@gmail.com>
2018-06-08 12:20:34 -0400

    Update
    -Added sessiondriver config to use Redis as a session store rather than the filesystem.
    -Added persistconnection config so we don't close out the session every time.

--- src/Config/config.php
+++ src/Config/config.php
@@ -2,7 +2,9 @@
 
 // vTiger API constants
 return [
-	'url' => 'path/to/vtiger/webservice',
-	'username' => '',
-	'accesskey' => '',
+    'url' => 'path/to/vtiger/webservice',
+    'username' => '',
+    'accesskey' => '',
+    'sessiondriver' => 'file', //reddis or file
+    'persistconnection' => true,
 ];
